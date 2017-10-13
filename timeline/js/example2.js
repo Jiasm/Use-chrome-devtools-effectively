fa21d1de6865771ce1df92466e3a1065865c52d0
jiashunming <jiashunming@outlook.com>
2017-10-13 09:50:20 +0800

    update

--- timeline/js/example2.js
+++ timeline/js/example2.js
@@ -1,1 +1,15 @@
-console.log('example2')
+window.addEventListener('load', function () {
+  document.querySelector('#btn').addEventListener('click', function () {
+    let count = 1000
+
+    // document.querySelector('#wrap').innerHTML = ''
+
+    let str = ''
+    while (count--) {
+      // document.querySelector('#wrap').innerHTML += `<p>${count}</p>`
+      str += `<p>${count}</p>`
+    }
+
+    document.querySelector('#wrap').innerHTML = str
+  })
+})
